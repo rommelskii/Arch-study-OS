043d41a31dde270bd17f198022f0e994588b8ed4
rommelskii <100370098+rommelskii@users.noreply.github.com>
2022-10-09 19:45:01 +0800

    Create apps.sh

---
+++ apps.sh
@@ -0,0 +1,32 @@
+#! \bin\bash
+
+sudo pacman -Syyu
+
+sudo pacman -S install git
+
+sudo pacman -S --needed base-devel
+
+cd /home/$(whoami)
+
+mkdir git
+
+cd git
+
+git clone https://aur.archlinux.org/libpamac-aur
+
+git clone https://aur.archlinux.org/pamac-aur
+
+cd libpamac-aur 
+
+makepkg -si
+
+cd /home/$(whoami)/git/pamac-aur
+
+makepkg -si 
+
+cd
+
+pamac build notion-app cider teams prospect-mail-bin 
+
+echo "Done"
+
