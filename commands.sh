21a3fc8e2334f973677f1b05105d87ab3f5c011a
rommelskii <100370098+rommelskii@users.noreply.github.com>
2022-12-31 00:09:18 +0800

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-sudo mv bin /usr/local/bin
+sudo mv conf /usr/local/bin/
+sudo mv install /usr/local/bin/
+sudo mv new /usr/local/bin/
+sudo mv pls /usr/local/bin/
+sudo mv stfu /usr/local/bin/
+sudo mv update /usr/local/bin/
